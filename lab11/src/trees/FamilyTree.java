e27d2365b8e4fc35c4481317e834502eb886b1dd
anthony <gunpotter@gmail.com>
2023-04-21 09:11:59 -0700

    updated TreeNode class

--- lab11/src/trees/FamilyTree.java
+++ lab11/src/trees/FamilyTree.java
@@ -5,6 +5,8 @@ import java.io.*;
 import javax.swing.*;
 import javax.swing.filechooser.*;
 
+import org.w3c.dom.Node;
+
 
 public class FamilyTree
 {
@@ -31,6 +33,7 @@ public class FamilyTree
         
         void addChild(TreeNode childNode)
         {
+        	children.add(childNode);
             // Add childNode to this node's children list. Also
             // set childNode's parent to this node.
         }
@@ -41,12 +44,17 @@ public class FamilyTree
         TreeNode getNodeWithName(String targetName)
         {
             // Does this node have the target name?
-            if (?????)
+            if (name.equals(targetName))
                 return this;
                     
             // No, recurse. Check all children of this node.
             for (TreeNode child: children)
             {
+            	TreeNode check = getNodeWithName(child);
+            	if(check!=null)
+            	{
+            		return check;
+            	}
                 // If child.getNodeWithName(targetName) returns a non-null node,
                 // then that's the node we're looking for. Return it.
             }
@@ -61,7 +69,12 @@ public class FamilyTree
         ArrayList<TreeNode> collectAncestorsToList()
         {
             ArrayList<TreeNode> ancestors = new ArrayList<>();
-
+            TreeNode current = this.parent;
+            while(current!=null)
+            {
+            	ancestors.add(current);
+            	current= current.parent;
+            }
             // ?????  Collect ancestors of this TreeNode into the array list. HINT: going up
             // the nodes of a tree is like traversing a linked list. If that isn’t clear,
             // draw a tree, mark any leaf node, and then mark its ancestors in order from
